83eac4027c684161075b627cef2de4b9a4e43589
Hiroki Tagato <tagattie@gmail.com>
2018-03-09 21:14:22 +0900

    Add script to generate CRL to Signing CA.

---
+++ signing-ca/01_generate_crl.sh
@@ -0,0 +1,26 @@
+#! /bin/sh
+
+export LANG=C
+export PATH=/bin:/sbin:/usr/bin:/usr/sbin:/usr/local/bin:/usr/local/sbin
+
+BASEDIR=$(cd "$(dirname "$0")" && pwd)
+TOPDIR=${BASEDIR}
+
+CA_NAME="signing-ca"
+
+CA_CONF=${CA_NAME}.cnf
+# CA_KEY=private/${CA_NAME}.key
+# CA_CSR=${CA_NAME}.csr
+# CA_CRT=${CA_NAME}.crt
+CA_CRL=crl/${CA_NAME}.crl
+
+cd "${TOPDIR}" || exit 1
+
+echo
+echo "### Generating certificate revocation list..."
+openssl ca \
+        -gencrl \
+        -config ${CA_CONF} \
+        -out ${CA_CRL}
+
+exit 0
